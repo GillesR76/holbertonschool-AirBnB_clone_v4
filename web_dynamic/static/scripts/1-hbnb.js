b7db6c08fb1b8d599d22f4518e574b9943c08804
GillesR76 <gilles-richard76@hotmail.com>
2024-05-28 13:31:25 +0200

    feat: Add dynamic checkbox functionality and API status check
    This commit adds dynamic checkbox functionality to the web application. When a checkbox is checked or unchecked, the corresponding amenity is added or removed from the checkedAmenities object. The selected amenities are then displayed in the amenities section of the page.
    
    Additionally, this commit includes an API status check. It makes a GET request to the status API and updates the availability status of the API based on the response.

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  let checkedAmenities = {};
+
+  $('input[type="checkbox"]').change(function () {
+    if (this.checked) {
+      checkedAmenities[$(this).data("id")] = $(this).data("name");
+    } else {
+      delete checkedAmenities[$(this).data("id")];
+    }
+
+    let lst = Object.values(checkedAmenities);
+    if (lst.length > 0) {
+      $("div.amenities h4").text(Object.values(checkedAmenities).join(", "));
+    } else {
+      $("div.amenities h4").html("&nbsp;");
+    }
+  });
+});
