0a25061b2ead4fe4ce145c9f704b5387c2f5e57f
GillesR76 <gilles-richard76@hotmail.com>
2024-05-30 09:49:45 +0200

    chore: Update checkbox handling and cache_id in 1-hbnb.js and 1-hbnb.html

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,18 +1,14 @@
 $(document).ready(function () {
-  let checkedAmenities = {};
+  const selectedAmen = {};
 
   $('input[type="checkbox"]').change(function () {
     if (this.checked) {
-      checkedAmenities[$(this).data("id")] = $(this).data("name");
+      selectedAmen[$(this).data("id")] = $(this).data("name");
     } else {
-      delete checkedAmenities[$(this).data("id")];
+      delete selectedAmen[$(this).data("id")];
     }
 
-    let lst = Object.values(checkedAmenities);
-    if (lst.length > 0) {
-      $("div.amenities h4").text(Object.values(checkedAmenities).join(", "));
-    } else {
-      $("div.amenities h4").html("&nbsp;");
-    }
+    const amenList = Object.values(selectedAmen).join(", ");
+    $("div.amenities h4").text(amenList);
   });
 });
