511509355e60e09a1884b1fd3f9f779bc8910ee8
GillesR76 <gilles-richard76@hotmail.com>
2024-05-31 12:15:45 +0200

    chore: Update script paths in HTML files

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,3 +1,43 @@
+function getPlaces(amenities = []) {
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:5001/api/v1/places_search',
+    data: JSON.stringify({amenities: amenities}),
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      // Loop through the data and create an article for each place
+      data.forEach(function (place) {
+        let article = `
+        <article>
+          <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">$${place.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${place.max_guest} Guest${
+          place.max_guest != 1 ? "s" : ""
+        }</div>
+            <div class="number_rooms">${place.number_rooms} Bedroom${
+          place.number_rooms != 1 ? "s" : ""
+        }</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathroom${
+          place.number_bathrooms != 1 ? "s" : ""
+        }</div>
+          </div>
+          <div class="description">
+            ${place.description}
+          </div>
+        </article>
+      `;
+
+        // Append the article to the places section
+        $("section.places").append(article);
+      });
+    },
+  });
+}
+
 $(document).ready(function () {
   let checkedAmenities = {};
 
@@ -18,7 +58,7 @@ $(document).ready(function () {
 
   // Make a GET request to the status API
   $.get(
-    "http://0.0.0.0:5001/api/v1/places_search/",
+    "http://localhost:5001/api/v1/status/",
     function (data, textStatus, jqXHR) {
       if (data.status === "OK") {
         // If the status is "OK", add the class "available" to the div#api_status
@@ -32,40 +72,8 @@ $(document).ready(function () {
 
   // When the button is clicked, make a POST request with the checked amenities
   $("button").click(function () {
-    $.post(
-      "http://0.0.0.0:5001/api/v1/places_search/",
-      JSON.stringify({ amenities: Object.keys(checkedAmenities) }),
-      function (data, textStatus, jqXHR) {
-        // Loop through the data and create an article for each place
-        data.forEach(function (place) {
-          let article = `
-          <article>
-            <div class="title_box">
-              <h2>${place.name}</h2>
-              <div class="price_by_night">$${place.price_by_night}</div>
-            </div>
-            <div class="information">
-              <div class="max_guest">${place.max_guest} Guest${
-            place.max_guest != 1 ? "s" : ""
-          }</div>
-              <div class="number_rooms">${place.number_rooms} Bedroom${
-            place.number_rooms != 1 ? "s" : ""
-          }</div>
-              <div class="number_bathrooms">${place.number_bathrooms} Bathroom${
-            place.number_bathrooms != 1 ? "s" : ""
-          }</div>
-            </div>
-            <div class="description">
-              ${place.description}
-            </div>
-          </article>
-        `;
-
-          // Append the article to the places section
-          $("section.places").append(article);
-        });
-      },
-      "json"
-    );
+    getPlaces(Object.keys(checkedAmenities));
   });
+
+  getPlaces();
 });
